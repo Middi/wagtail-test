d4aeccded9759ae275e04beacf9985950bcda6bf
Middi <richard@richardmiddleton.me>
2020-02-07 17:07:39 +0000

    setup homepage template

--- home/models.py
+++ home/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    # Home Page Model
+    templates = "templates/home/home_page.html"
+
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title")
+    ]
+
+    class Meta:
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
